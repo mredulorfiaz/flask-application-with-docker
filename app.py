9e38a5611df32083e0e4cdb122a41c3650380423
Orfiaj Karim <gazi.karim@newscred.com>
2021-05-27 07:41:50 +0600

    Add User Model

--- app.py
+++ app.py
@@ -6,8 +6,28 @@ from config import Config
 app = Flask(__name__)
 app.secret_key = "My Secret Key"
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.DB_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fName = db.Column(db.String(50))
+    lName = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    mobile = db.Column(db.Integer(15), unique=True, nullable=False)
+    isDeleted = db.Column(db.Boolean())
+
+    def __init__(self, fName, lName, email, mobile, isDeleted):
+        self.fName = fName
+        self.lName = lName
+        self.email = email
+        self.mobile = mobile
+        self.isDeleted = isDeleted
+
 
 @app.route('/')
 def index():
